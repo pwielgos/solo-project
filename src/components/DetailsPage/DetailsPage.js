cace3d0a9d49bdedd2c72a1c7847a68aa861d46b
pwielgos <paigewielgos@gmail.com>
2020-02-19 10:02:54 -0600

    finished dropdown menu to add images to a gallery

--- src/components/DetailsPage/DetailsPage.js
+++ src/components/DetailsPage/DetailsPage.js
@@ -2,11 +2,17 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 class DetailsPage extends Component {
+    componentDidMount = () => {
+        this.props.dispatch({
+            type: 'GET_USER_GALLERIES',
+        })
+    }
+
     handleClick = (imageUrl) => {
         this.props.dispatch({ type: 'POST_IMAGE', payload: imageUrl })
     }
 
-    takeToResults = () =>{
+    takeToResults = () => {
         this.props.history.push('/search')
     }
 
@@ -17,21 +23,15 @@ class DetailsPage extends Component {
                     <button onClick={this.takeToResults}>Back</button>
                 </header>
                 <img src={`${this.props.reduxState.detail}/full/full/0/default.jpg`} />
-                
-                {/* come back to this dropdown functionality later */}
-
-                <label for="galleries">Add to gallery</label>
+                <label for="galleries">Add to gallery:</label>
                 <select id="galleries">
-                    <option value="gallery1">Monet</option>
-                    <option value="gallery2">photographs</option>
-                    <option value="gallery3">cats</option>
-                    <option value="newGallery">Create New Gallery +</option>
+                    {this.props.reduxState.gallery.map(gallery => {
+                        return (
+                            <option value={gallery.gallery_name}>{gallery.gallery_name}</option>
+                        )
+                    })}
                 </select>
-
                 <button onClick={() => this.handleClick(this.props.reduxState.detail)}>Add</button>
-                {/* {JSON.stringify(this.props.reduxState.detail.url)} */}
-                {/* {JSON.stringify(this.props.reduxState.detail)} doesn't have anything useful besides the url */}
-                {/* {JSON.stringify(this.props.reduxState.search)} */}
             </div>
         )
     }
